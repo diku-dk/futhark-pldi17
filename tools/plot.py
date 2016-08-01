6be2c246fb54289c3b72c17d472b29cb6ce9252d
Troels Henriksen <athas@sigkill.dk>
2016-08-01 11:42:02 +0200

    Better plotting.

--- tools/plot.py
+++ tools/plot.py
@@ -33,19 +33,34 @@ finpar_programs = [("LocVolCalib", "LocVolCalib_large"),
 def plotting_info(x):
     name, filename = x
     with open(os.path.join('runtimes', filename + '.speedup')) as f:
-        return (name, float(f.read()))
+        speedup = float(f.read())
+        aux_speedup = 0.0
+        try:
+            with open(os.path.join('aux_runtimes', filename + '.speedup')) as f:
+                aux_speedup = float(f.read())
+        except:
+            aux_speedup = 0.0
+        return (name, speedup, aux_speedup)
 
-to_plot = map(plotting_info, rodinia_programs + accelerate_programs + finpar_programs)
+to_plot = map(plotting_info, rodinia_programs + finpar_programs + accelerate_programs)
 
-program_names = [ name for (name, _) in to_plot ]
-program_speedups = [ speedup for (_, speedup) in to_plot ]
+program_names = [ name for (name, _, _) in to_plot ]
+program_speedups = [ speedup for (_, speedup, _) in to_plot ]
+program_aux_speedups = [ aux_speedup for (_, _, aux_speedup) in to_plot ]
 
 N = len(to_plot)
 
-# the width of the bars
+# the widths of the bar
 width = 0.5
+def compute_space(x):
+    if x[2] == 0.0:
+        return width*2
+    else:
+        return width*3
+spaces = np.array(map(compute_space, to_plot))
+
 # the x locations for the bars
-ind = np.arange(N)+width
+ind = width+np.concatenate(([0], np.cumsum(spaces)[:-1]))
 
 fig, ax = plt.subplots()
 
@@ -55,9 +70,9 @@ plt.rc('font', **font)
 grey='#aaaaaa'
 
 ax.set_ylim([0.0,6.0])
-ax.set_xlim([0.0,ind[-1]+width*2])
+ax.set_xlim([0.0,ind[-1]+1])
 ax.set_ylabel('Speedup')
-ax.set_xticks(ind+width/2)
+ax.set_xticks(ind+width)
 ax.set_xticklabels(program_names)
 ax.xaxis.set_ticks_position('none')
 ax.yaxis.set_ticks_position('none')
@@ -79,16 +94,27 @@ for t in ax.get_xticklabels( ):
     else:
         stagger = 0
 
-rects = ax.bar(ind, program_speedups, width, color='#0033cc', zorder=3)
+rects = ax.bar(ind, program_speedups, width, color='#2255ee', zorder=3, edgecolor='white')
+aux_rects = ax.bar(ind+width, program_aux_speedups, width, color='#ee5500', zorder=3, edgecolor='white')
 
 def label_rect(rect):
-    height = min(6.0, rect.get_height())
-    ax.text(rect.get_x() + rect.get_width()/2., 1.05*height+0.1,
+    height = rect.get_height()
+
+    if height == 0.0:
+        return
+
+    font = {'family': 'sans-serif',
+            'weight': 'bold',
+            'size': 8,
+    }
+    bounded_height = min(6.0, height)
+    ax.text(rect.get_x() + rect.get_width(), 1.05*bounded_height,
             '%.2f' % height,
-            ha='center', va='bottom')
+            ha='center', va='bottom', fontdict=font, rotation=45)
 
 map(label_rect, rects)
+map(label_rect, aux_rects)
 
 fig.set_size_inches(8.5, 1.5)
-plt.rc('text', usetex=True)
+plt.rc('text')
 plt.savefig(outputfile, bbox_inches='tight')
